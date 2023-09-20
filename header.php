d39fc2b004aee9be562d4f9f619ad939b3b2b5f6
VojtechKohout06 <145565928+VojtechKohout06@users.noreply.github.com>
2023-09-20 15:23:31 +0200

    Update header.php
    forgot to add lightbox links

--- header.php
+++ header.php
@@ -5,7 +5,10 @@
         <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width, viewport-fit=cover">
         <link rel="stylesheet" href="<?php echo $cssTitle ?>" type="text/css" />
-        <link rel="shortcut icon" href="/img/android-chrome-384x384-2-1-150x150.ico" />
+        <link rel="shortcut icon" href="img/android-chrome-384x384-2-1-150x150.ico"/>
+        <link rel="stylesheet" href="css/lightbox.min.css" type="text/css" media="screen" />
+        <script src="js/lightbox-plus-jquery.min.js"></script>
+        <script src="js/lightbox.min.js"></script>
         <title>Praga</title>
     </head>
     <body>
