07fcda03a06950b4b4bbb7ed345cd3642892a25f
VojtechKohout06 <145565928+VojtechKohout06@users.noreply.github.com>
2023-10-31 19:17:02 +0100

    Update Dealers.php

--- Dealers.php
+++ Dealers.php
@@ -26,7 +26,7 @@
                 </div>
                 <div class="dealers">
                     <h1>Our dealers:</h1>
-                    <p>The best dealers of vědomosti and(or) piko</p>
+                    <p>The best dealers of knowledge</p>
                     <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2573.310554447657!2d13.900073215898086!3d49.83662203928311!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x470b0760b2aadf89%3A0x2bc0b5f6dfee3b04!2zU0_FoCBhIFNPVSBIb8WZb3ZpY2U!5e0!3m2!1scs!2scz!4v1679334714057!5m2!1scs!2scz" 
                     width="90%" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
                 </div>
